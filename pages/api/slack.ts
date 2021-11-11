7d52197dfe7fb56116a63eb7e3f75c3418579ac9
Christopher Hand <soccerdebater@gmail.com>
2021-11-11 16:04:35 -0600

    Add a slack endpoint

---
+++ pages/api/slack.ts
@@ -0,0 +1,29 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { PepService } from '../../services/pep';
+
+type PepTalk = {
+  message: string
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<PepTalk>
+) {
+  res.status(200);
+  
+  let payload = req.body;
+
+  let channel = payload.event.item.channel;
+
+  fetch('https://slack.com/api/chat.postMessage',{
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    headers: {
+      'Authorization': `Bearer ${process.env.SLACK_BOT_TOKEN}`,
+    },
+    body: JSON.stringify({
+        "text": PepService.generatePep(),
+        "channel": channel
+    })
+    })
+    .then(response => response.json())
+}
