c6459795d31729206b6d413941e26dcc673911ab
Christopher Hand <soccerdebater@gmail.com>
2021-11-11 16:29:30 -0600

    Response after posting

--- pages/api/slack.ts
+++ pages/api/slack.ts
@@ -1,33 +1,27 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { PepService } from '../../services/pep';
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<string>
 ) {
-  res.status(200).send('');
-  
-  let payload = req.body;
+    let payload = req.body;
 
-  console.log(payload);
+    console.log(payload);
 
-  let channel = payload.channel_id;
+    let channel = payload.channel_id;
 
-  fetch('https://slack.com/api/chat.postMessage',{
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    headers: {
-      'Authorization': `Bearer ${process.env.SLACK_BOT_TOKEN}`,
-    },
-    body: JSON.stringify({
-        "text": PepService.generatePep(),
-        "channel": channel
+    await fetch('https://slack.com/api/chat.postMessage',{
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        headers: {
+        'Authorization': `Bearer ${process.env.SLACK_BOT_TOKEN}`,
+        },
+        body: JSON.stringify({
+            "text": PepService.generatePep(),
+            "channel": channel
+        })
     })
-    })
-    .then(response => response.json())
-    .then(r => {
-        console.log(r);
-    })
-    .catch(e => {
-        console.log(e);
-    });
+    .then(response => response.json());
+
+    res.status(200).send('');
 }
