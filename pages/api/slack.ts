a39417f98c352689acb9ce51800f133beb66712f
Christopher Hand <soccerdebater@gmail.com>
2021-11-11 16:22:51 -0600

    That works, but not well, let's post to the channel

--- pages/api/slack.ts
+++ pages/api/slack.ts
@@ -5,5 +5,29 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<string>
 ) {
-  res.status(200).send(PepService.generatePep());
+  res.status(200).send('');
+  
+  let payload = req.body;
+
+  console.log(payload);
+
+  let channel = payload.channel_id;
+
+  fetch('https://slack.com/api/chat.postMessage',{
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    headers: {
+      'Authorization': `Bearer ${process.env.SLACK_BOT_TOKEN}`,
+    },
+    body: JSON.stringify({
+        "text": PepService.generatePep(),
+        "channel": channel
+    })
+    })
+    .then(response => response.json())
+    .then(r => {
+        console.log(r);
+    })
+    .catch(e => {
+        console.log(e);
+    });
 }
